d1d1505fc66f903a2aedc8699a369d3329164dce
EunjungSong <unjyon.song@gmail.com>
2025-07-28 15:41:47 +0900

    gsap 설치, navigation.tsx 스타일 수정

--- src/components/Layout/Navigation.jsx
+++ src/components/Layout/Navigation.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 const Nav = styled.nav`
   background-color: #ffffff;
   padding: 1rem 2rem;
+  width: 100%;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 
   @media (max-width: 768px) {
